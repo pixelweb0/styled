10a16b362bb6083798cb5a79573b944dde543c8d
pixelweb0 <pixelweb@naver.com>
2023-04-25 17:39:25 +0900

    브런치 연습

---
+++ branch.js
@@ -0,0 +1,17 @@
+1. 브런치 생성
+	git branch 브런치명
+
+2. 브런치 전환
+	git checkout 브런치명
+
+
+
+※ B브런치
+	git add .
+	git commit -m "커밋메시지"
+		git push
+
+※ A브런치
+	git checkout A	// A브런치로 변환
+	git merge B  		// B브런치를 합친다
+
